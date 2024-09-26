3d7316b01f70b6907629b9092096115fd2dd242a
HaramSs <haram8274@gmail.com>
2024-09-26 18:29:50 +0900

    9/25

---
+++ programmers/두 수의 연산값 비교하기.py
@@ -0,0 +1,8 @@
+def solution(a, b):
+    q = str(a) + str(b)
+    answer = 0
+    return answer
+
+print(solution(2, 91))
+print(q)
+
--- programmers/수 조작하기1.py
+++ programmers/수 조작하기1.py
@@ -11,3 +11,13 @@ def solution(n, control):
             n -= 10
         
     return n
+
+
+
+
+def solution(n, control):
+    answer = n
+    c = { 'w':1, 's':-1, 'd':10, 'a':-10}
+    for i in control:
+        answer += c[i]
+    return answer

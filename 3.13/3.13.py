5964ed505e1fce8475b6ab7d0092bf2ebc55ec0e
HaramSs <haram8274@gmail.com>
2025-03-13 22:19:07 +0900

    3/13

---
+++ 3.13/3.13.py
@@ -0,0 +1,96 @@
+# x = 10
+# if x == 10:
+#     print('10입니다.')
+
+# x = 10
+# if x == 10:
+#     pass
+
+# if x == 10:
+#     pass # TODO: x가 10일 떄 처리가 필요함
+
+# x = 10
+# if x == 10:
+#     print('x에 들어있는 숫자는')
+#     print('10입니다.')
+
+# x = 15
+
+# if x >= 10:
+#     print('10 이상입니다.')
+
+#     if x == 15:
+#         print('15입니다.')
+
+#     if x == 20:
+#         print('20입니다.')
+
+
+# x = int(input())
+# if x == 10:
+#     print('10입니다.')
+
+# if x ==20:
+#     print('20 입니다.')
+
+# x = 5
+# if x == 10:
+#     print('10 입니다.')
+# else:
+#     print('10이 아닙니다.')
+
+# x = 5
+
+# if x == 10:
+#     print('10입니다.')
+# else:
+#     print('x에 들어있는 숫자는')
+#     print('10이 아닙니다.')
+
+# if True:
+#     print('참')
+# else :
+#     print('거짓')
+
+# if False:
+#     print('참')
+# else:
+#     print('거짓')
+
+# if None:
+#     print('참')
+# else:
+#     print('거짓')
+
+# if 0:
+#     print('참')
+# else:
+#     print('거짓')   # 0은 거짓
+
+# if 1:
+#     print('참')
+# else:
+#     print('거짓')   # 1은 참
+
+# if 0x1F: # 16진수
+#     print('참')
+# else:
+#     print('거짓') # 참
+
+# if 13.5: # 실수
+#     print('참')
+# else:
+#     print('거짓')   # 참
+
+
+# if 'Hello': # 문자열
+#     print('참') # 문자열은 참
+# else:
+#     print('거짓')
+
+# if '': # 빈 문자열
+#     print('참')
+# else:
+#     print('거짓')   # 빈문자열은 거짓
+
+
